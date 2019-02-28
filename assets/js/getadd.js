1f1237b7cd65799f7010dccef216aa63f39f8c0e
octav <octav@behigh.es>
2019-03-01 01:09:27 +0300

    Reformat code

--- assets/js/getadd.js
+++ assets/js/getadd.js
@@ -1,7 +1,7 @@
 import {UniqueId, webgl} from './utils/index'
 
 ((win, doc) => {
-	const KEY = 'getadd_cb';
+	const KEY = 'getadd_cb'
 	const URL = '/getadd'
 
 	const getAdd = (id, target) => {
@@ -10,39 +10,39 @@ import {UniqueId, webgl} from './utils/index'
 			uid: UniqueId(),
 			location: doc.location.href,
 			referer: doc.referrer,
-			tz: (win.Intl &&  Intl.DateTimeFormat().resolvedOptions().timeZone) || '',
+			tz: (win.Intl && Intl.DateTimeFormat().resolvedOptions().timeZone) || '',
 			tz_offset: (new Date).getTimezoneOffset() * -1,
 			plugins: ((win.navigator && Array.prototype.slice.apply(navigator.plugins)) || []).map(plugin => plugin.name).join(','),
 			cookie: (win.navigator && navigator.cookieEnabled) || false,
 			webgl_hash: webgl()
 		}
 
-		data.cb = '_cb_' + data.uid;
+		data.cb = '_cb_' + data.uid
 
 		let link = URL + '?' + Object.keys(data).map(k => encodeURIComponent(k) + "=" + encodeURIComponent(data[k])).join('&')
 
 		win[data.cb] = (data) => {
 			try {
-				const a = doc.createElement('a')
-				const img = a.appendChild(doc.createElement('img'))
-				const dst = doc.getElementById(target)
+				const a       = doc.createElement('a')
+				const img     = a.appendChild(doc.createElement('img'))
+				const dst     = doc.getElementById(target)
 				dst.innerHTML = ''
 				dst.appendChild(a)
-				a.href = data.link
+				a.href   = data.link
 				a.target = '_blank'
-				img.alt = ''
-				img.src = data.src
+				img.alt  = ''
+				img.src  = data.src
 			} catch (e) {
 			}
 		}
 
 		// Use script json for cross domain support
-		const tag = doc.getElementsByTagName('script')[0]
+		const tag    = doc.getElementsByTagName('script')[0]
 		const script = doc.createElement('script')
-		script.src = link
-		script.type = 'text/javascript'
-		script.async = true;
-		tag.parentNode.insertBefore(script, tag);
+		script.src   = link
+		script.type  = 'text/javascript'
+		script.async = true
+		tag.parentNode.insertBefore(script, tag)
 	}
 
 	/**
