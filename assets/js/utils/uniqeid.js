97380f4981774e0fef3a05f6290a9970a51a67e8
octav <octav@behigh.es>
2019-03-01 01:25:10 +0300

    Simplify syntax

--- assets/js/utils/uniqeid.js
+++ assets/js/utils/uniqeid.js
@@ -1,8 +1,6 @@
 const UniqueId = (length = 8) => {
 
-	const getRandomInt = (min, max) => {
-		return Math.floor(Math.random() * (max - min + 1)) + min
-	}
+	const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 
 	const parts = (+new Date).toString().split('').reverse()
 	const parts_length = parts.length
